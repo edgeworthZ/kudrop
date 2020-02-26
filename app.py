9a59892f904c9d29cd770f3dc724ee4ab0269db3
lazycodex <Hope_Of_dfinal@hotmail.com>
2020-02-26 16:07:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', s
 client = gspread.authorize(creds)
 sheet = client.open("KUDrop").sheet1
 
-dialogues = ['ฮาร์ดแวร์หน้าหี']
+dialogues = ['หน้าหี','ประยุทธ์หัวควย','ประยวยหัวคุด']
 
 app = Flask(__name__)
 
